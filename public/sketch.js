6e9eee4727054b92d07c3cebf5255120fe3d518e
kyamisuke <murakami.k.am@m.titech.ac.jp>
2020-12-08 19:42:58 +0900

    my first commit

---
+++ public/sketch.js
@@ -0,0 +1,56 @@
+var socket;
+
+function setup() {
+    createCanvas(600, 400);
+    background(51);
+
+    socket = io.connect('http://localhost:3000');
+    socket.on('mouse', newDrawing);
+    socket.on('ZIGSIM/SyuFyMxNClLa0loa/touch01', newDeviceDrawing);
+    socket.on('pads', newOperated);
+}
+
+function newDrawing(data) {
+    noStroke();
+    fill(255, 0, 100);
+    ellipse(data.x,data.y,30,30);
+}
+
+function newDeviceDrawing(data) {
+    console.log("iPhone is coming!! data: " + data);
+}
+
+function mouseDragged() {
+    console.log('Sending: ' + mouseX + ',' + mouseY);
+
+    var data = {
+        x: mouseX,
+        y: mouseY
+    }
+
+    socket.emit('mouse', data);
+
+    noStroke();
+    fill(255);
+    ellipse(mouseX,mouseY,30,30);
+}
+
+function controllerOperated(pads) {
+    console.log(pads);
+
+    socket.emit('pads', pads);
+}
+
+function newOperated(data) {
+    console.log(data);
+}
+
+function draw() {
+    var pads = navigator.getGamepads ? navigator.getGamepads() :
+    (navigator.webkitGetGamepads ? navigator.webkitGetGamepads : []);
+
+    pads = pads[0];
+    if (pads) {
+        controllerOperated(pads);
+    }
+}
---
+++ server.js
@@ -0,0 +1,40 @@
+
+var express = require('express');
+
+var app = express();
+var server = app.listen(3000);
+
+app.use(express.static('public'));
+
+console.log("My socket server is running");
+
+var socket = require('socket.io');
+
+var io = socket(server);
+
+io.sockets.on('connection', newConnection);
+
+function newConnection(socket) {
+    console.log('new connection: ' + socket.id)
+
+    socket.on('mouse', mouseMsg);
+    socket.on('/ZIGSIM/SyuFyMxNClLa0loa/touch01', iosMsg);
+    socket.on('pads', controllerMsg);
+
+    function mouseMsg(data) {
+        socket.broadcast.emit('mouse', data);
+        console.log(data);
+    }
+
+    function iosMsg(data) {
+        socket.broadcast.emit('ZIGSIM/SyuFyMxNClLa0loa/touch01', data);
+        console.log(data);
+    }
+
+    function controllerMsg(data) {
+        socket.broadcast.emit('pads', data);
+        console.log(data);
+    }
+
+
+}
